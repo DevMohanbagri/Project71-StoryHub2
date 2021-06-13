727afe82d06c38604979e62927129e692faf1c99
DevMohanbagri <73389635+DevMohanbagri@users.noreply.github.com>
2021-06-13 12:49:59 +0530

    Update ReadStoryScreen.js

--- screens/ReadStoryScreen.js
+++ screens/ReadStoryScreen.js
@@ -1,49 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View ,Image, TouchableOpacity} from 'react-native';
-import db from '../config';
-import firebase from 'firebase';
 
 export default class ReadStory extends React.Component{
-    constructor(){
-        super()
-        this.state={
-          author: '',
-          title: '',
-          story: ''
-        }
-    }
-
-        fieldDataHandler = async ()=>{
-            const storypPage = db.collection('stories').doc('story')
-
-            const doc = await storypPage.get();
-
-            if (!docT.exists && !docA.exists && !docS.exists) {
-                alert('No such document!');
-            } 
-            else{
-                this.setState({
-                    author: authorDB,
-                    title: titleDB,
-                    story: storyDB
-                })
-            }   
-            
-        }
-
     render(){
         return(
             <View>
-                <TouchableOpacity onPress={()=>{
-                    this.fieldDataHandler();
-                }}>
+                
                 <Text>ReadStory</Text>
-                </TouchableOpacity>
-                <Text>{this.state.title}</Text>
-                <Text>{this.state.story}</Text>
-                <Text>{this.state.author}</Text>
+                
             </View>
         )
     }
-
 }
